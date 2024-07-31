1ad27288c4d06c09d64a6eebb6d68a594cf02c50
shl0mo <scc@icomp.ufam.edu.br>
2024-07-31 15:31:42 -0400

    feat: dice container style
    and screen style adapted to the dice container

--- components/loginScreen/initPlaying.js
+++ components/loginScreen/initPlaying.js
@@ -1,5 +1,7 @@
 function initPlaying () {
     const loginScreen = document.querySelector('.login-container')
+    const screen = document.querySelector('.screen')
     loginScreen.remove()
+    screen.classList.add('screen-dice-container-grid')
     createDiceContainer()
 }
